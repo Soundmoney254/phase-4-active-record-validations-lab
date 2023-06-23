dcec48d45a4b9578f1064891a994f9843e70a12b
Samuel <samnmbuguah@gmail.com>
2023-06-23 15:17:39 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
+    # Custom validator
+    validate :clickbait
+    def clickbait
+        clickbait_words = %w(Won't Believe Secret Top Guess)
+        if title && !clickbait_words.any? { |word| title.include?(word) }
+            errors.add(:title, "must contain one of the following clickbait words: #{clickbait_words.join(', ')}")
+        end
+      end
+      
+  end
+  
